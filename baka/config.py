7e4b80d9d650de5dae54b5aa7edbab29d3b7356f
cyberpravin2 <pravin709121@gmail.com>
2026-02-09 01:17:53 +0530

    Update config.py

--- baka/config.py
+++ baka/config.py
@@ -44,7 +44,7 @@ GIT_TOKEN = os.getenv("GIT_TOKEN", "")
 # Images & Links
 START_IMG_URL = os.getenv("START_IMG_URL", "https://telegra.ph/file/5e5480760e412bd402e88.jpg") 
 HELP_IMG_URL = os.getenv("HELP_IMG_URL", "https://files.catbox.moe/5g37fy.jpg") 
-WELCOME_IMG_URL = os.getenv("WELCOME_IMG_URL", "https://files.catbox.moe/gyi5iu.jpg") 
+WELCOME_IMG_URL = os.getenv("WELCOME_IMG_URL", "https://freeimage.host/i/fpoGcWQ") 
 
 SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/YourSupportGroup")
 SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/YourUpdateChannel")
